8da0af40dcfff76763d1574e3830c1c1590d1dcf
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-07-09 12:37:32 -0400

    Successful call to /api/login, set token to be stored in localStorage

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FriendsList = () => {
+
+  return(
+    <div>
+      <h2>Your Friends</h2>
+      <button>Add a Friend</button>
+    </div>
+  )
+};
+
+export default FriendsList;
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,11 +1,11 @@
 import React, { useState }from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 const Login = () => {
 
   const [credentials, setCredentials] = useState({
       username: '',
-      password: ''
+      password: '',
   })
 
   const handleChange = e => {
@@ -15,13 +15,19 @@ const Login = () => {
     })
   }
 
-  // handleLogin = e => {
-
-  // }
+  const handleLogin = e => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload)
+        console.log("Logged In!")
+      })
+      .catch(err =>console.log("Please input the correct credentials", err))
+  }
 
     return(
         <div>
-            <form>
+            <form onSubmit={handleLogin}>
                 <input 
                   type="text"
                   name="username"
