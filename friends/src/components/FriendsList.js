cf44092c0c4608b460408996da1c3e230ff8e2a4
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-07-09 21:48:07 -0400

    successfull api call for FriendsList.js

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect}  from 'react';
+
+import axiosWithAuth from '../helpers/axiosWithAuth';
 
 const FriendsList = () => {
+
+const [friendsList, setFriendsList] = useState([])
+
+useEffect(() =>{
+  axiosWithAuth().get("/friends")
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
+}); 
 
   return(
     <div>
