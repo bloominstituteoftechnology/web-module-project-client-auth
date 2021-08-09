7f7e947ddc84d237554656be8fb8b19258a50d1f
Greg Brewton <gbrewton1980@gmail.com>
2021-08-08 21:33:32 -0600

    adds logout button/function

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { axiosWithAuth } from "../axiosWithAuth";
 
 const FriendsList = (props) => {
   // const [list, setList] = useState([]);
-  return <div>FriendsList component</div>;
+  const history = useHistory();
+  const logout = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/logout")
+      .then(() => {
+        localStorage.removeItem("token");
+        history.push("/login");
+      });
+  };
+
+  return (
+    <div>
+      <button onClick={logout}>Log Out</button>
+    </div>
+  );
 };
 
 export default FriendsList;
