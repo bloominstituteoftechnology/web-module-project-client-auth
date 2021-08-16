703c06fe73ec53aff2561585b21e19e23162cde2
Rick <rmansfield@realpeoplesratings.com>
2021-08-16 16:03:33 -0500

    add FriendList.js

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+class FriendsList extends React.Component {
+    state = {
+        friends: []
+    };
+        
+    componentDidMount() {
+        console.log("Friends componentDidMount 5x5");
+        this.getData();
+    }
+
+    getData = () => {
+        axiosWithAuth()
+        .get("/friends")
+        .then((res)=>{
+            this.setState({friends: res.data});
+            console.log('');
+        })
+    }
+    render() {
+        const  { friends } = this.state;
+        return (
+            <div className="friends-container">
+                {friends.map (friend => {
+                    return(
+                    <div key={friend.id} className="friend">
+                        <p>Name: {friend.name}, {friend.age}</p>
+                        <p>email: {friend.email}</p>
+                    </div>
+                )})}
+            </div>
+        )};
+    }
+    export default FriendsList;
