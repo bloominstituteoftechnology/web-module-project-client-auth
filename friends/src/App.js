b96271ad2672122f02d74a65870cd4c9a5bfd8a7
Rick <rmansfield@realpeoplesratings.com>
2021-08-16 10:11:06 -0500

    Start Link for Login in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+// import Login from "./components/Login";
 
 import './App.css';
 
 function App() {
+
+
   return (
+    <Router>
     <div className="App">
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
 
     </div>
+    </Router>
   );
 }
 
