7e1d330251a2aa3096bfd099896395b17ff24526
Rick <rmansfield@realpeoplesratings.com>
2021-08-16 16:01:07 -0500

    Update App.js with Switch, PrivateRoutes & imports

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-// import Login from "./components/Login";
+import Login from "./components/Login";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
+import PrivateRoute from "./components/PrivateRoute";
+import FriendsList from "./components/FriendsList";
 
 import './App.css';
 
-function App() {
+  function App() {
+    const logout = () => {
+      axiosWithAuth()
+        .post("/logout")
+        .then((res) => {
+          localStorage.removeItem("token");
+          // console.log(window);
+          window.location.href = "/login";
+        })
+        .catch((err) => console.log(err));
+    };
 
-
-  return (
-    <Router>
-    <div className="App">
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-
-    </div>
-    </Router>
-  );
-}
+    return (
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link onClick={logout}>Logout</Link>
+            </li>
+            <li>
+              <Link to="/protected">Friends List</Link>
+            </li>
+          </ul>
+          <Switch>
+            <PrivateRoute exact path="/protected" component={FriendsList} />
+            <Route path="/login" component={Login} />
+            <Route component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
 
 export default App;
