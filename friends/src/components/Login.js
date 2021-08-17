12af20ed7391352c6b5ed194769790f87a773ecc
Rick <rmansfield@realpeoplesratings.com>
2021-08-17 08:55:28 -0500

    added signing credentials to initial state

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -4,8 +4,8 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 class Login extends React.Component {
     state = {
         credentials: {
-            username: "",
-            password: "",
+            username: "lambda",
+            password: "school",
         },
     };
 
