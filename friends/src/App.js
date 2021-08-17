4cb21f5c90a010fdd97654f6a0d999f9e19ad329
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-17 14:26:15 -0700

    logincommit

---
+++ friends/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {BrowserRouter as Router, Route,Link,Switch} from 'react-router-dom'
+import Login from './components/Login'
+function App() {
+  return ( 
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/login" component={Login}/>
+        </Switch>
+    </div>
+    </Router>
+
+    
+  );
+}
+
+export default App;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    state= {
+        credentials:{
+            username: '',
+            password: ''
+        }
+    };
+
+    handleInput = e =>{
+        this.setState({
+            credentials:{
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    login = e =>{
+        e.preventDefault();
+        axios.post('http://localhost:5000/api/login', this.state.credentials)
+        .then(res =>{
+            localStorage.setItem("token", res.data.token)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                    type="text"
+                    name="username"
+                    value={this.state.credentials.username}
+                    onChange={this.handleInput}/>
+                    <input
+                    type="password"
+                    name="password"
+                    value={this.state.credentials.password}
+                    onChange={this.handleInput}/>
+                    <button>Login</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default Login;
