aa43033c38f046f24c6f634fd29da466806abd1a
unknown <j.ordonez419@gmail.com>
2021-09-03 19:57:49 -0500

    form hooked up to state

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,26 +1,42 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 const Login = (props) => {
 
     const [isLoading, setIsLoading] = useState(false)
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
 
     const handleChanges = (e) => {
-
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
     }
     const handleSubmit = (e) => {
         e.preventDefault()
+        // setIsLoading(!isLoading)
+    }
+
+    const login = () => {
+
     }
 
     return (
         <div>
+            <h1> {isLoading ? (
+                <p>Loading...</p>
+            ) : ('')}</h1>
             <form>
                 <label htmlFor="username">
-                    <input type="text" placeholder="Enter Username" />
+                    <input name='username' onChange={handleChanges} type="text" placeholder="Enter Username" value={credentials.username} />
                 </label>
                 <label htmlFor="password">
-                    <input type="password" placeholder="Enter Password" />
+                    <input name='password' type="password" onChange={handleChanges} placeholder="Enter Password" value={credentials.password} />
                 </label>
-                <button>Submit</button>
+                <button onClick={handleSubmit}>Submit</button>
             </form>
         </div>
     )
