c8675cda9eeedceb42ed586fabff01c268a1bb83
Richard Oh <n24512@protonmail.com>
2021-09-14 14:21:52 -0500

    post returning data

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -7,7 +7,44 @@ const Login = () => {
     password: "",
   });
 
-  return <div></div>;
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
