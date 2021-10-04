b4a43073fd2648588f64365fc331d570dc189e5a
vida <ofo28@aol.com>
2021-10-04 01:11:54 -0400

    Done

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { axiosWithAuth } from './utils/axiosWithAuth';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 class Login extends React.Component {
     state = {
--- friends/src/components/Logout.js
+++ friends/src/components/Logout.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {axiosWithAuth} from "./utils/axiosWithAuth";
+import axiosWithAuth from "../utils";
 
 const Logout = (props)=> {
     useEffect(() => {
@@ -7,7 +7,7 @@ const Logout = (props)=> {
         .post("/api/logout", {
         }).then(res => {
             localStorage.removeItem('token');
-            //localStorage.removeItem('username');
+            localStorage.removeItem('username');
             window.location.pathname = '/login'
         }).catch(err => {
             console.log(err);
