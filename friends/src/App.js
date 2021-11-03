f1e5cc2d926c03ba68202e49b25bef2f06ae71b3
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-11-03 00:32:13 -0500

    link and route are set up

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,21 @@
 import Login from './components/Login'
+import { Link, Route, Switch } from 'react-router-dom'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login />
-      </header>
+      <Link to='/login'> Login </Link>
+      <Link to='/'> Home </Link>
+        <div>
+          <Switch>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/'></Route>
+          </Switch>
+        </div>
     </div>
   );
 }
