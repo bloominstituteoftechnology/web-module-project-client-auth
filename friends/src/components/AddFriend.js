430dc311b2748b6cd4458c1df847f42b291d9ae8
djho57 <daniel.j.ho57@gmail.com>
2021-11-10 01:32:59 -0600

    mvp

---
+++ friends/src/components/AddFriend.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const initialCred = {
+    id: 0,
+    name: '',
+    age: 0,
+    email: ''
+}
+
+const AddFriend = (props) => {
+    const [cred, setCred] = useState(initialCred)
+
+    const handleChange = e => {
+        setCred({
+            ...cred,
+            [e.target.name]:e.target.value
+        });
+    }
+
+const add = () => {
+    const token = localStorage.getItem('token');
+    console.log(cred);
+    axios.post('http://localhost:5000/api/friends', cred, {headers: {authorization:token}})
+    .then(res => {
+        console.log(res)
+        setCred(res.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+return(
+    <div className='add-friend'>
+        <form onSubmit={add}>
+            <label>Name
+                <input type='text' name='name' value={cred.name} onChange={handleChange} />
+            </label>
+            <label>Age
+                <input type='number' name='age' value={cred.age} onChange={handleChange} />
+            </label>
+            <label>Email
+                <input type='email' name='email' value={cred.email} onChange={handleChange} />
+            </label>
+            <button className='add-submit'>ADD</button>
+        </form>
+    </div>
+)
+}
+export default AddFriend;
---
+++ friends/src/components/Logout.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from 'react';
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+const Logout = (props)=> {
+    useEffect(() => {
+        axiosWithAuth()
+        .post("/api/logout", {
+        }).then(res => {
+            localStorage.removeItem('token');
+            localStorage.removeItem('username');
+            props.history.push("/login")
+        }).catch(err => {
+            console.log(err);
+        })
+    }, [])
+    return(<div></div>);
+}
+
+export default Logout;
