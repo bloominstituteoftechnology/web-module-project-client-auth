5739ce30d41061ea79c0de241c2fdca0ce01547e
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-11-09 19:56:55 -0500

    added axiosWithAuth and logout

--- friends/src/components/Logout.js
+++ friends/src/components/Logout.js
@@ -1,4 +1,23 @@
+import React, { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+
+import axiosWithAuth from "../utils/axiosWithAuth";
+
 const Logout = (props)=> {
-    return (<div></div>)
+    const {push} = useHistory();
+
+    useEffect(()=> {
+        const token = localStorage.getItem("token");
+
+        axiosWithAuth()
+            .post('/logout')
+            .then(resp => {
+                localStorage.removeItem('token');
+                push('/login');
+            });
+    }, []);
+    
+    return(<div></div>);
 }
+
 export default Logout;
