1acbd3eadec49af396393dc2a4f8b49535321c29
Terri Archer <territangela@gmail.com>
2022-12-31 14:10:53 -0500

    fixed login link

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
                 <h1>FRIENDS DATABASE</h1>
             </div>
             <nav>
-                <Link to='/login'><button>LOGIN.</button></Link>
+                <Link to='/'><button>LOGIN.</button></Link>
                 <Link to='/friendlist'><button>FRIENDLIST.</button></Link>
                 <Link to='/addfriend'><button>ADDFRIEND.</button></Link>
                 <Link to='/logout'><button>LOGOUT.</button></Link>
